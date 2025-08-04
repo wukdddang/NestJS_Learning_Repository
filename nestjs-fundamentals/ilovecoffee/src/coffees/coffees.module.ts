85fd00e7fb8b718148970c1644c15ea01020df7c
= <wukddang@gmail.com>
2025-08-04 17:16:22 +0900

    NestJS Fundamentals: 8 fin.

--- nestjs-fundamentals/ilovecoffee/src/coffees/coffees.module.ts
+++ nestjs-fundamentals/ilovecoffee/src/coffees/coffees.module.ts
@@ -1,11 +1,10 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
-import { COFFEE_BRANDS } from './coffees.constants';
 import { ConfigModule } from '@nestjs/config';
 import coffeesConfig from './config/coffees.config';
 
@@ -15,14 +14,7 @@ import coffeesConfig from './config/coffees.config';
     ConfigModule.forFeature(coffeesConfig),
   ],
   controllers: [CoffeesController],
-  providers: [
-    CoffeesService,
-    {
-      provide: COFFEE_BRANDS,
-      useFactory: () => ['buddy brew', 'nescafe'],
-      scope: Scope.TRANSIENT,
-    },
-  ],
+  providers: [CoffeesService],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
---
+++ nestjs-fundamentals/ilovecoffee/test/coffee/coffees.e2e-spec.ts
@@ -0,0 +1,73 @@
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+
+describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'pass123',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    await app.init();
+  });
+
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        const expectedCoffee = expect.objectContaining({
+          ...coffee,
+          flavors: expect.arrayContaining(
+            coffee.flavors.map((flavor) =>
+              expect.objectContaining({ name: flavor }),
+            ),
+          ),
+        });
+        expect(body).toEqual(expectedCoffee);
+      });
+  });
+  it.todo('Get all [GET /]');
+  it.todo('Get one [GET /:id]');
+  it.todo('Update one [PATCH /:id]');
+  it.todo('Delete one [DELETE /:id]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
