a3b6c52a250ffad31a11f917dd375926ff88d55f
= <wukddang@gmail.com>
2025-08-04 13:07:21 +0900

    NestJS Fundamentals: 6 fin.

--- nestjs-fundamentals/ilovecoffee/src/coffees/coffees.controller.ts
+++ nestjs-fundamentals/ilovecoffee/src/coffees/coffees.controller.ts
@@ -13,6 +13,8 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -20,21 +22,28 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
+    console.log(protocol);
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     // const { limit, offset } = paginationQuery;
 
     return this.coffeeService.findAll(paginationQuery);
   }
 
+  @Public()
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    console.log(typeof id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    // console.log(typeof id);
+    console.log(id);
     return this.coffeeService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    // console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeeService.create(createCoffeeDto);
   }
 
--- nestjs-fundamentals/ilovecoffee/src/main.ts
+++ nestjs-fundamentals/ilovecoffee/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
+// import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,10 @@ async function bootstrap() {
         enableImplicitConversion: true,
       },
     }),
+  );
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    // new TimeoutInterceptor(),
   );
   await app.listen(process.env.PORT ?? 4000);
 }
