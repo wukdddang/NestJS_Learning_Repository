086b279b7c5342ef980b7bde2b90f225b9a53a2a
= <wukddang@gmail.com>
2025-08-05 09:19:14 +0900

    NestJS Fundamentals: 9-7 fin.

--- nestjs-fundamentals/ilovecoffee/src/coffees/coffees.service.ts
+++ nestjs-fundamentals/ilovecoffee/src/coffees/coffees.service.ts
@@ -5,6 +5,7 @@ import { Model } from 'mongoose';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -12,8 +13,9 @@ export class CoffeesService {
     @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
   ) {}
 
-  findAll() {
-    return this.coffeeModel.find().exec();
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
+    return this.coffeeModel.find().skip(offset).limit(limit).exec();
   }
 
   async findOne(id: string) {
