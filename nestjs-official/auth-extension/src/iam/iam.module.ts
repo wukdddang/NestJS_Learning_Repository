9805f9f8dccecb2fe40fc123056df6a94d72a5c2
= <wukddang@gmail.com>
2025-08-18 19:00:36 +0900

    NestJS Auth/Authz: 2-6 fin.

--- nestjs-official/auth-extension/src/iam/iam.module.ts
+++ nestjs-official/auth-extension/src/iam/iam.module.ts
@@ -8,6 +8,9 @@ import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token.guard';
+import { AuthenticationGuard } from './authentication/guards/authentication.guard';
 
 @Module({
   imports: [
@@ -20,6 +23,11 @@ import jwtConfig from './config/jwt.config';
       provide: HashingService,
       useClass: BcryptService,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    AccessTokenGuard,
     AuthenticationService,
   ],
   exports: [HashingService],
