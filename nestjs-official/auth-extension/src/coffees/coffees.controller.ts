0ca8d04382c7d56a536ce7f6ac8b672e769d3e20
= <wukddang@gmail.com>
2025-08-19 18:58:50 +0900

    NestJS Auth/Authz: 2-7 fin.

--- nestjs-official/auth-extension/src/coffees/coffees.controller.ts
+++ nestjs-official/auth-extension/src/coffees/coffees.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -13,7 +24,7 @@ export class CoffeesController {
   }
 
   @Get()
-  findAll() {
+  findAll(@ActiveUser() user: ActiveUserData) {
     return this.coffeesService.findAll();
   }
 
